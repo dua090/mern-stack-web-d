6c4abf0160ded068824971a1ca98306a4a8c9b27
dua090 <102320261+dua090@users.noreply.github.com>
2025-02-24 13:15:33 +0530

    Update Resume.js

--- fullstackprojects/resumeextracter/backend/src/models/Resume.js
+++ fullstackprojects/resumeextracter/backend/src/models/Resume.js
@@ -1,11 +1,18 @@
+
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const ResumeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String },
-    skills: [String],
-    preferences: String,
-}, { collection: "Resume" });  // 👈 This ensures collection is "Resume"
-
-const Resume = mongoose.model("Resume", ResumeSchema);
-export default Resume;
+dotenv.config();
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
