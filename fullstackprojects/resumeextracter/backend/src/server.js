a729188192449078e19f5cb4623f9e6606133500
dua090 <102320261+dua090@users.noreply.github.com>
2025-02-24 13:11:56 +0530

    Update server.js

--- fullstackprojects/resumeextracter/backend/src/server.js
+++ fullstackprojects/resumeextracter/backend/src/server.js
@@ -9,38 +9,18 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
-methods: ["POST", "GET"],
-credentials: true
-    }
-));
+app.use(
+    cors({
+      origin: "*", // Allow all origins (for testing)
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type"],
+    })
+  );
+  
 app.use(express.json());
 
 app.use("/api", resumeRoutes);
 app.use("/api", jobRoutes);
-app.post("/upload", async (req, res) => {
-    try {
-        const result = await parseResume(req.file.buffer);
-
-        console.log("Extracted Data:", result.extractedData); // Debugging step
-
-        if (result && result.extractedData) {
-            const newResume = new Resume(result.extractedData);
-            await newResume.save();
-            console.log("Data inserted successfully!");
-            res.json({ success: true, data: result.extractedData });
-        } else {
-            console.error("No extracted data found!");
-            res.status(400).json({ error: "No extracted data" });
-        }
-    } catch (error) {
-        console.error("Error inserting data:", error);
-        res.status(500).json({ error: "Database save failed" });
-    }
-});
-
 console.log(process.env.PORT)
 const PORT = process.env.PORT||5001 ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
