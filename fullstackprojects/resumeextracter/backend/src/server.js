5cf2db8c73b898e19e0af9122d662eb1c9c2ea03
dua090 <102320261+dua090@users.noreply.github.com>
2025-02-24 13:22:54 +0530

    Update server.js

--- fullstackprojects/resumeextracter/backend/src/server.js
+++ fullstackprojects/resumeextracter/backend/src/server.js
@@ -9,13 +9,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(
-    cors({
-      origin: "*", // Allow all origins (for testing)
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type"],
-    })
-  );
+app.use(cors());
   
 app.use(express.json());
 
