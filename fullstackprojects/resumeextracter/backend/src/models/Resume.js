5fd71a5ed3e82f1278fe10b693ebe8dfdef451c9
schollprop <getpackproof@gmail.com>
2025-05-13 01:07:37 +0530

    all changes are done

--- fullstackprojects/resumeextracter/backend/src/models/Resume.js
+++ fullstackprojects/resumeextracter/backend/src/models/Resume.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-=======
-// import mongoose from "mongoose";
-
-// const ResumeSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     email: { type: String },
-//     skills: [String],
-//     preferences: String,
-// }, { collection: "Resume" });  // 👈 This ensures collection is "Resume"
-
-// const Resume = mongoose.model("Resume", ResumeSchema);
-// export default Resume;
->>>>>>> ace7726 (Your commit message here)
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
@@ -29,8 +14,4 @@ export const connectDB = async () => {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> ace7726 (Your commit message here)
